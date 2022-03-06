6e1c88e03b211a89a497e88625a3ca30bf4b60ec
karthikt <karthik.thatipelly96@gmail.com>
2022-03-06 12:29:21 +0530

    main.js added

---
+++ main.js
@@ -0,0 +1,6 @@
+let viewText = document.getElementById('viewtext');
+let theText = document.getElementById('thetext');
+function showInfo() {
+     theText.style.display ="block";
+}
+viewText.addEventListener('mouseover', showInfo)
